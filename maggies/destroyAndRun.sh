df8ca32f9612af92f5890ea3e14442a452abb94f
ExogenesisBen <ben.d.jackson@outlook.com>
2016-11-06 00:39:26 +0000

    Added scripts to clean run and run

---
+++ maggies/destroyAndRun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo "Installing requirements"
+pip install -r requirements.txt
+rm -r -f db.sqlite3
+
+echo ""
+echo "Migrating db"
+python manage.py migrate --run-syncdb   # don't create superuser
+
+echo ""
+echo "About to populate"
+python populate.py  # run population script to add admin
+
+echo ""
+echo "The server is about to run..."
+echo "3, 2, 1..."
+
+python2.7 manage.py runserver
