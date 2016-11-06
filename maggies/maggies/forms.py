431294e2b8a5cc61be5299483766af169b04c3d1
Lily Roberts <lilyroberts95@gmail.com>
2016-11-06 09:02:56 +0000

    Edited help text on main form

--- maggies/maggies/forms.py
+++ maggies/maggies/forms.py
@@ -28,10 +28,10 @@ class ActivityForm(forms.Form):
 class VisitorForm(forms.Form):
     visitor_name = forms.CharField(label="Visitor's First Name", max_length=100, help_text="This will help identify this person on their travels today only.")
     visitor_type = forms.ChoiceField(choices=VISITOR_TYPE_CHOICES, label="Visitor Type")
-    is_new_visitor = forms.BooleanField(required=False, label="First visit?", help_text="Has this person visited before today?")
+    is_new_visitor = forms.BooleanField(required=False, label="First visit?", help_text="Was this their first visit?")
     visitor_gender = forms.ChoiceField(choices=GENDER_CHOICES, label="Gender")
-    nature_of_visit = forms.ModelChoiceField(VisitNature.objects.all(), label="Nature of Visit", help_text="Why did this person visit today?")
-    cancer_site = forms.ModelChoiceField(CancerSite.objects.all(), label="Cancer Site", help_text="What type of cancer does the person deal with? Select 'Not Stated' if it did not come up in conversation, or 'Unknown Primary' if even their doctor doesn't know.")
+    nature_of_visit = forms.ModelChoiceField(VisitNature.objects.all(), label="Nature of Visit", help_text="How did the person visit the centre today? (e.g. did they have an appointment or just drop in?)")
+    cancer_site = forms.ModelChoiceField(CancerSite.objects.all(), label="Cancer Site", help_text="Where is the site of the person's cancer? Select 'Not Stated' if it did not come up in conversation, or 'Unknown Primary' if even their doctor doesn't know.")
     journey_stage = forms.ModelChoiceField(JourneyStage.objects.all(), label="Journey Stage")
 
     # Secret hidden fields used for tracking
